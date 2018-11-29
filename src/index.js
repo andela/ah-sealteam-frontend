5350a3eff52d1c0c212bf76f21dc694e6a7823f7
Bluey95 <blue.were@yahoo.com>
2018-11-29 19:05:06 +0300

    [Chore #161278859] Setup redux

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
 import configureStore from "./store";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
