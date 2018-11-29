5ca39881849af4f862b562f90be14cd18c406e3b
Bluey95 <blue.were@yahoo.com>
2018-11-29 19:05:06 +0300

    [Chore #161278859] Setup redux

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,7 @@ import '../assets/styles/App.scss';
 import { connect } from "react-redux";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -27,4 +28,4 @@ class App extends Component {
 const mapStateToProps = state => ({
   ...state
 });
-export default connect()(App);
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.scss';
-import { Provider } from "react-redux";
-import configureStore from "./store";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import Home from './containers/Home';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <Home />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
