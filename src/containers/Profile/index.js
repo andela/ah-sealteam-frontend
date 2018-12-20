94a1792a4d976bd5ddc2781f79c9c06dc683860f
Lazarus Odhiambo <lazarusodhis@gmail.com>
2018-12-21 02:59:08 +0300

    Fix(imageUpload/loader): - implement loader on the upload button [Delivers #162801752]

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -5,9 +5,11 @@ import 'font-awesome/css/font-awesome.min.css';
 import '../Profile/index.css';
 import ProfileUpdate from './ProfileUpdate';
 import { bindActionCreators } from 'redux';
-import Loader from '../../components/Profile';
+// import Loader from '../../components/Profile';
 import uploadToCloudinary from '../../utils/cloudinary';
 import BookMark from '../BookMark';
+import Loader from 'react-dots-loader';
+import 'react-dots-loader/index.css';
 
 class Profile extends Component {
     state = {
@@ -51,7 +53,7 @@ class Profile extends Component {
         const { data, fetching, fetched } = this.props.profile;
         const info = (
             <React.Fragment>
-                <div className="container-all">
+                <div className="container">
                     <div className="row mt-4">
                         {/* {Profile image Start} */}
                         <div className="col-md-4 mt-4">
@@ -79,7 +81,14 @@ class Profile extends Component {
                                             onChange={this.handleImageUpload}
                                         />
                                         <span className="btn btn-light btn-block">
-                                            Update Image
+                                            {this.state.loading ? (
+                                                <Loader
+                                                    size={10}
+                                                    distance={31}
+                                                />
+                                            ) : (
+                                                'Update Image'
+                                            )}
                                         </span>
                                     </label>
                                 </figure>
@@ -87,13 +96,6 @@ class Profile extends Component {
                                 <h6 className="mt-1">{`${
                                     data.firstname ? data.firstname : ''
                                 } ${data.lastname ? data.lastname : ''}`}</h6>
-                            </div>
-                            <div
-                                className={
-                                    this.state.loading ? 'lds-rolling' : ''
-                                }
-                            >
-                                <div />
                             </div>
                             <br />
                         </div>
@@ -234,7 +236,11 @@ class Profile extends Component {
                 <BookMark />
             </React.Fragment>
         );
-        return <div>{fetching && !fetched ? <Loader /> : info}</div>;
+        return (
+            <div className="mt-4 ml-4">
+                {fetching && !fetched ? <Loader /> : info}
+            </div>
+        );
     }
 }
 
